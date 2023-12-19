9fe111de2ba020a2380e2b574b792e74a396b932
qka10777 <qka10777@gmail.com>
2023-12-19 09:27:15 +0900

    drama

--- src/main/java/com/korea/MOVIEBOOK/Drama/DramaController.java
+++ src/main/java/com/korea/MOVIEBOOK/Drama/DramaController.java
@@ -1,16 +1,20 @@
 package com.korea.MOVIEBOOK.Drama;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-
 public class DramaController {
 
     private final DramaService dramaService;
@@ -19,8 +23,17 @@ public class DramaController {
     public String dramaList (Model model) {
         List<Drama> dramaList = dramaService.dramaList();
         model.addAttribute("dramaList", dramaList);
-        return "drama_list";
+        return "drama/drama_list";
     }
 
+    @GetMapping("/drama/{id}")
+    public String dramaDetail(@PathVariable Long id, Model model) {
+        Drama drama = dramaService.getDramaById(id);
+        model.addAttribute("drama", drama);
+        return "drama/drama_detail";
+    }
 
 }
+
+
+
--- src/main/java/com/korea/MOVIEBOOK/Drama/DramaService.java
+++ src/main/java/com/korea/MOVIEBOOK/Drama/DramaService.java
@@ -1,9 +1,25 @@
 package com.korea.MOVIEBOOK.Drama;
 
+import jdk.dynalink.beans.StaticClass;
 import lombok.RequiredArgsConstructor;
+import nz.net.ultraq.thymeleaf.layoutdialect.models.ElementMerger;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.util.ObjectUtils;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
+import java.util.Stack;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +31,12 @@ public class DramaService {
         return dramaRepository.findAll();
     }
 
+    public Drama getDramaById(Long id) {
+        return dramaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("해당 영화는 존재하지 않습니다. " + id));
+    }
 
 }
+
+
+
