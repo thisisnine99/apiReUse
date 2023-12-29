028097aa93db1aa89e4a7b157283df9b24ee9cf5
thisisnine99 <dkdlel2099@gmail.com>
2023-12-29 11:06:04 +0900

    bookMainPageMapping완료

--- src/main/java/com/korea/MOVIEBOOK/book/BookController.java
+++ src/main/java/com/korea/MOVIEBOOK/book/BookController.java
@@ -36,39 +36,11 @@ public class BookController {
             endIndex+=5;
         }
         System.out.println("=================================== 새로고침 ===================================");
-        model.addAttribute("bestSellerListList", bestSellerListList);
-        model.addAttribute("newSpecialBookListList", newSpecialBookListList);
-        return "book/bookMainPage";
-    }
-
-
-    @GetMapping("/test")
-    public String test(Model model) {
-        List<Book> bestSellerList = bookService.getBestSellerList();
-        bestSellerList.sort(Comparator.comparing(Book::getBestRank));
-        List<List<Book>> bestSellerListList = new ArrayList<>();
-        int startIndex = 0;
-        int endIndex = 5;
-        for (int i = 1; i <= bestSellerList.size()/5; i++) {
-            bestSellerListList.add(bestSellerList.subList(startIndex, Math.min(endIndex, bestSellerList.size())));
-            startIndex+=5;
-            endIndex+=5;
-        }
-        startIndex = 0;
-        endIndex = 5;
-        List<Book> newSpecialBookList = bookService.getNewSpecialBookList();
-        List<List<Book>> newSpecialBookListList = new ArrayList<>();
-        for (int i = 1; i <= newSpecialBookList.size()/5; i++) {
-            newSpecialBookListList.add(newSpecialBookList.subList(startIndex, Math.min(endIndex, newSpecialBookList.size())));
-            startIndex+=5;
-            endIndex+=5;
-        }
-        System.out.println("=================================== 새로고침 ===================================");
         List<List<List<Book>>> allList = new ArrayList<>();
         allList.add(bestSellerListList);
         allList.add(newSpecialBookListList);
         model.addAttribute("allList", allList);
-        return "book/bookTest";
+        return "book/bookMainPage";
     }
 
     @PostMapping("/detail")
@@ -76,5 +48,4 @@ public class BookController {
         model.addAttribute("title", title);
         return "book/testtest";
     }
-
 }
